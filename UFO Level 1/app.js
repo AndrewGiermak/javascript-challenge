e54c9e3e4beeb8573dd0b4a193eaf4e75a3055b8
Andrew Giermak <64851573+AndrewGiermak@users.noreply.github.com>
2020-09-09 11:24:51 -0400

    Update app.js

--- UFO Level 1/app.js
+++ UFO Level 1/app.js
@@ -1,4 +1,41 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
+
 
 // YOUR CODE HERE!
+// Function to build the table
+function NewTable(tableData) {
+    // Clear existing data
+  tbody.html("");
+    // Loop and append rows and cells 
+  tableData.forEach((dataRow) => {
+    var row = tbody.append("tr");
+   Object.values(dataRow).forEach((val) => {
+     var cell = row.append("td");
+       cell.text(val);
+     }
+   );
+ });
+}
+
+// Function to search the table by date/time
+function SearchTable() {
+    // Prevent Refresh 
+    d3.event.preventDefault();
+    // Get the datetime value entered by user
+    var date = d3.select("#datetime").property("value");
+    let searchData = tableData;
+    // Filter by datetime
+    if (date) {
+      searchData = searchData.filter(row => row.datetime === date);
+    }
+   // Build table based on search data
+    NewTable(searchData);
+  }
+  // Event statement for button click 
+  d3.selectAll("#filter-btn").on("click", SearchTable;
+  
+  // Default Loading Page 
+  NewTable(tableData);
+  
